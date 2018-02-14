446f8fcd8e3793e87715e1b7eb3b6022c3c8938b
Octave Muhirwa <omuhirwa@LOCAL-01-LT-134.local>
2018-02-13 20:19:19 -0500

    Initial commit

---
+++ Social App/Controller/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social App
+//
+//  Created by Octave Muhirwa on 2/9/18.
+//  Copyright © 2018 Octave Muhirwa. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
