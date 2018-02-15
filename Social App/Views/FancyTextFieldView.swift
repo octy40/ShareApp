0deb17683bbc8505f85b54ffa009af6c83ff46b6
Octave Muhirwa <omuhirwa@lt-076.corp.localytics.com>
2018-02-15 08:24:14 -0500

    Added custom text field views

---
+++ Social App/Views/FancyTextFieldView.swift
@@ -0,0 +1,28 @@
+//
+//  FancyTextFieldView.swift
+//  Social App
+//
+//  Created by Octave Muhirwa on 2/15/18.
+//  Copyright © 2018 Octave Muhirwa. All rights reserved.
+//
+
+import UIKit
+
+class FancyTextFieldView: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
