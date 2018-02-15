600e908f0e35616a3180a0473f4494d08f4a50b9
Octave Muhirwa <omuhirwa@lt-076.corp.localytics.com>
2018-02-15 08:04:14 -0500

    Added 2 views

---
+++ Social App/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Social App
+//
+//  Created by Octave Muhirwa on 2/14/18.
+//  Copyright © 2018 Octave Muhirwa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Social App/Views/RoundBtn.swift
@@ -0,0 +1,21 @@
+//
+//  RoundBtn.swift
+//  Social App
+//
+//  Created by Octave Muhirwa on 2/15/18.
+//  Copyright © 2018 Octave Muhirwa. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
